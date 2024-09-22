22e80b2b4727f71d4b2a85ad0ec6fa497c973976
harsh12uv <harsharaj2099@gmail.com>
2024-09-22 20:05:51 +0530

    t5

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-      <p>
+     <div id="main">
+       <p>
       I am learning React. My life is getting better.
       </p>
+     </div>
   )
 }
 
